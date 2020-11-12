94671910c0942d7989e165695518f1369f8cc67f
jbernalr <40041819+jhonhenrybernal@users.noreply.github.com>
2020-11-11 19:40:18 -0500

    component vue plan

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('all-plans', require('../js/components/plans/list.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        currentActivity: 'home'
+    }
+});
--- resources/views/plans/list.blade.php
+++ resources/views/plans/list.blade.php
@@ -1,3 +1,4 @@
+ <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -8,8 +9,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
-       </div>
+                <div id="app">
+                    <all-plans></all-plans>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
+<script src="{{ asset('js/app.js') }}" defer></script>
